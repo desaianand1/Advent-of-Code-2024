9aec4db85b35d54923f80eb9c5698e1d830240b9
Anand Desai <desaianand456@gmail.com>
2024-12-01 10:40:21 +0530

    chore: implemented template utilities for day generation

---
+++ utilities/template.kt
@@ -0,0 +1,17 @@
+package aocYEAR.dayDAY
+
+import utilities.readInput
+
+fun main() {
+    val input = readInput("YEAR/dayDAY/input.txt")
+    println("Part 1: ${part1(input)}")
+    println("Part 2: ${part2(input)}")
+}
+
+fun part1(input: List<String>): Int {
+    return 0  // TODO: Implement solution
+}
+
+fun part2(input: List<String>): Int {
+    return 0  // TODO: Implement solution
+}
---
+++ utilities/template_test.kt
@@ -0,0 +1,33 @@
+package aocYEAR.dayDAY
+
+import utilities.readInput
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class DayDAYTest {
+    private val testInput = """
+        // TODO: Add test input here
+    """.trimIndent().split("\n")
+    
+    @Test
+    fun `Part 1 with test input`() {
+        assertEquals(0, part1(testInput))  // TODO: Update expected value
+    }
+    
+    @Test
+    fun `Part 1 with actual input`() {
+        val input = readInput("YEAR/dayDAY/input.txt")
+        assertEquals(0, part1(input))  // TODO: Update with actual answer
+    }
+    
+    @Test
+    fun `Part 2 with test input`() {
+        assertEquals(0, part2(testInput))  // TODO: Update expected value
+    }
+    
+    @Test
+    fun `Part 2 with actual input`() {
+        val input = readInput("YEAR/dayDAY/input.txt")
+        assertEquals(0, part2(input))  // TODO: Update with actual answer
+    }
+}
