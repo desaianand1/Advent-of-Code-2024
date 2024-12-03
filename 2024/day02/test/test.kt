bf8de52fe72c5f4b5bf237934e696a1dc515bc8a
Anand Desai <desaianand456@gmail.com>
2024-12-03 18:57:08 +0530

    feat: completed day 02 solution

---
+++ 2024/day02/test/test.kt
@@ -0,0 +1,32 @@
+package aoc2024.day02
+
+import org.junit.jupiter.api.Test
+import utilities.readInput
+import kotlin.test.assertEquals
+
+class Day02Test {
+    private val testInput = readInput("2024/day02/test/test_input.txt")
+    private val realInput = readInput("2024/day02/input.txt")
+
+    @Test
+    fun `Part 1 with test input`() {
+        assertEquals(2, part1(testInput))
+    }
+
+    @Test
+    fun `Part 1 with actual input`() {
+        if (realInput.isNullOrEmpty()) return
+        assertEquals(516, part1(realInput))
+    }
+
+    @Test
+    fun `Part 2 with test input`() {
+        assertEquals(4, part2(testInput))
+    }
+
+    @Test
+    fun `Part 2 with actual input`() {
+        if (realInput.isNullOrEmpty()) return
+        assertEquals(561, part2(realInput))
+    }
+}
